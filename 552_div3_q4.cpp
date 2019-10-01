445a2798cde0713c56a84e688d7e4870db3b61fc
anakindoug2 <56033912+anakindoug2@users.noreply.github.com>
2019-10-01 19:33:46 +0530

    Add files via upload

---
+++ 552_div3_q4.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+	long long n,a,b,a_max,b_max;;
+	cin>>n>>b>>a;
+  a_max=a;
+  b_max=b;
+  int arr[n];
+  for (long i=0;i<n;i++)
+    cin>>arr[i];
+  long st=0;
+  for(int i=0;i<n;i++)
+  {
+    if(b>0||a>0)
+    {
+      if(a==a_max)
+      a--;
+      else if(arr[i]>0)
+      {
+        if(a==a_max)
+        a--;
+        else if(b>0){
+          b--;
+          a++;
+        }
+        else
+        a--;
+      }
+      else if(arr[i]==0&&a>0)
+        a--;
+      else if(arr[i]==0&&b>0)
+        b--;
+      else
+      break;
+      st++;
+    }
+    else
+    {
+      break;
+    }
+  }
+  cout<<st;
+
+return 0;
+}
